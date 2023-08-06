c8a0d56389e42c83739d54c46ca307ac271dfe4c
WenrongZou <wenrongzou@outlook.com>
2023-08-06 23:05:03 +0800

    Prop1.4

--- AlgebraicNumberTheory/ValuationTheory/ThePadicNumbers.lean
+++ AlgebraicNumberTheory/ValuationTheory/ThePadicNumbers.lean
@@ -5,6 +5,9 @@ import Mathlib.NumberTheory.Padics.PadicNumbers
 import Mathlib.Topology.MetricSpace.Completion
 import Mathlib.NumberTheory.Padics.RingHoms
 import Mathlib.Data.ZMod.Basic
+import Mathlib.RingTheory.MvPolynomial.Basic
+import Mathlib.Data.Polynomial.Eval
+import Mathlib.Data.Polynomial.Basic
 
 variable {p : ℕ} [Fact p.Prime]
 
@@ -20,4 +23,27 @@ variable {p : ℕ} [Fact p.Prime]
 -- The universal property of ℤ_[p] as projective limit. (Commutative Diagram)
 #check PadicInt.lift_unique
 #check PadicInt.lift_spec
+
+/- Proposition 1.2 : The residue classes a mod p^n ∈ ℤ⧸p^nℤ can be uniquely
+represented in the form 
+   
+-/
+#check PadicInt.lift_spec
+
+
+/- Proposition 1.4 : The F (x_1, … , x_n) be a polynomial with integer coeffients,
+  and fix a prime number 
+-/
+
+theorem not_monotone_iff {f : ℝ → ℝ} : ¬Monotone f ↔ ∃ x y, x ≤ y ∧ f x > f y := by
+  rw [Monotone]
+  push_neg
+  rfl
+
+
+
+#check Subring.mk
+
+theorem Prop1_4 (f : Type _) (f : Polynomial ℤ ) : (∀ v : ℕ, ∃ u : ℤ, (↑(p^v) : ℤ) ∣ Polynomial.eval u f) 
+  ↔ ∃ y : ℤ_[p], Polynomial.eval y (f.ofSubring (fun xs => sorry)) = 0 := sorry
 
